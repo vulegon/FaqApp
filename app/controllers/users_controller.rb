7c657afd160c93119dd1eb22bd07c4d0439d868c
github vukegon <walkurepqrt@gmail.com>
2021-11-17 19:41:46 +0900

    ユーザー登録フォームの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render "new"
+    end
+  end
+
+private
+
+  def user_params
+    #マスアサインメント対策 :user属性の名前、メールアドレス、パスワード、パスワードの確認属性のみ許可する
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
