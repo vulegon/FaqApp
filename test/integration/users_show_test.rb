e1da9c777414c08d7fa059b66dd675cdc47e646f
github vukegon <walkurepqrt@gmail.com>
2021-12-19 21:53:19 +0900

    user_showのエラー修正

--- test/integration/users_show_test.rb
+++ test/integration/users_show_test.rb
@@ -1,5 +1,12 @@
 require "test_helper"
 
 class UsersShowTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
 
+  test "ログインしていない状態でユーザー詳細画面に入ったときのテスト" do
+    get  user_path(@user)
+  end
 end
